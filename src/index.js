cfc703def63b303af9eabb9e2d30ceca239159ee
milenadiamonds1 <milenadubova16@gmail.com>
2023-12-11 18:52:27 +0200

    Revert "redeploy"
    This reverts commit 524746ca6ef7af265032bc82198df02fa742df1f.
    
    delete#	deleted:    node_modules/array.prototype.tosorted/package.json

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 import App from './App';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
   <Provider store={store}>
     <App />
-  </Provider>,
-  document.getElementById('root')
+  </Provider>
 );
-
